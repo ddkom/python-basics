06072a1e4c45773066cf761688b31fb17e416807
ddkom <52631087+ddkom@users.noreply.github.com>
2020-03-26 23:44:29 -0300

    Add files via upload

---
+++ Basics-strings.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+# Define a string 
+
+'Hello world'
+
+
+# In[8]:
+
+
+# or, define a string
+
+"Hello world"
+
+
+# In[22]:
+
+
+# Assign string to a variable
+
+A = "Hello world"
+print(A)
+
+
+# In[12]:
+
+
+# Determine length of string
+
+len(A)
+
+
+# In[14]:
+
+
+# Determine element of 6th index in variable A
+
+print(A[6])
+
+
+# In[17]:
+
+
+# Determine index of element "e" in variable A
+
+print(A.index("e"))
+
+
+# In[25]:
+
+
+# Take the slice on variable A with only index 0 to index 3
+
+A[0:3]
+
+
+# In[27]:
+
+
+# Get every second element. The elments on index 1, 3, 5 ...
+
+A[::2]
+
+
+# In[28]:
+
+
+# Get every second element in the range from index 0 to index 4
+
+A[0:5:2]
+
+
+# In[ ]:
+
+
+
+
